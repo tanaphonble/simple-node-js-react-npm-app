389a337d4f21c77d3f6ab6d2848a73506c353ac9
Tanaphon Auanhinkong <a.tanaphon@gmail.com>
2018-05-03 14:05:54 +0700

    update jenkinsfile

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -22,8 +22,8 @@ set +x
 set -x
 # npm start &
 sleep 1
-echo $! > .pidfile
-set +x
+# echo $! > .pidfile
+# set +x
 
 # echo 'Now...'
 # echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
